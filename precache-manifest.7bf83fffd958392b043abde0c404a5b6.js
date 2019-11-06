7350d97ea2c4cba6dacf72d9b9c17d6fde54a198
NguyenTheMan <luvzk12345@gmail.com>
2019-11-06 18:21:27 +0700

    Updates

---
+++ precache-manifest.7bf83fffd958392b043abde0c404a5b6.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03fa7efbf565fbbe65134b9df137e8c4",
+    "url": "/ElearningReact/index.html"
+  },
+  {
+    "revision": "4b37d0199c73ea3b5e1f",
+    "url": "/ElearningReact/static/css/2.9ba9c4c1.chunk.css"
+  },
+  {
+    "revision": "f5533bfe98bce4fd09ee",
+    "url": "/ElearningReact/static/css/main.28aae352.chunk.css"
+  },
+  {
+    "revision": "4b37d0199c73ea3b5e1f",
+    "url": "/ElearningReact/static/js/2.000eeede.chunk.js"
+  },
+  {
+    "revision": "f5533bfe98bce4fd09ee",
+    "url": "/ElearningReact/static/js/main.cfd0ffaa.chunk.js"
+  },
+  {
+    "revision": "291046ce18bb6c697cc6",
+    "url": "/ElearningReact/static/js/runtime~main.c5d4265d.js"
+  },
+  {
+    "revision": "79e7684c16995aff995c34a4574f2b81",
+    "url": "/ElearningReact/static/media/1.79e7684c.jpg"
+  },
+  {
+    "revision": "f63b6bfb24ab7b26b355a0856da37203",
+    "url": "/ElearningReact/static/media/2.f63b6bfb.jpg"
+  },
+  {
+    "revision": "71bf5eb7e18dd9f3170c6e9cbb4ff869",
+    "url": "/ElearningReact/static/media/4.71bf5eb7.jpg"
+  },
+  {
+    "revision": "009754a5be4e8fc806e03fd7cf0a3c2f",
+    "url": "/ElearningReact/static/media/5.009754a5.jpg"
+  },
+  {
+    "revision": "da1a1de0bc1bf700ebcd24cb1557293a",
+    "url": "/ElearningReact/static/media/HeartRed.da1a1de0.png"
+  },
+  {
+    "revision": "3a38cfd4f3b361926a92051e2e43910c",
+    "url": "/ElearningReact/static/media/Like.3a38cfd4.png"
+  },
+  {
+    "revision": "46d3c0bb79e1ad308c110e73e182edba",
+    "url": "/ElearningReact/static/media/Send.46d3c0bb.png"
+  },
+  {
+    "revision": "c103b03625e7cfbd0c926aaff8533ade",
+    "url": "/ElearningReact/static/media/Video.c103b036.png"
+  },
+  {
+    "revision": "61d332540e79db02ed11edeabb42692e",
+    "url": "/ElearningReact/static/media/avatar_1.61d33254.jpg"
+  },
+  {
+    "revision": "5e389c7a63ad36e27214e0423d6ffc77",
+    "url": "/ElearningReact/static/media/avatar_2.5e389c7a.jpg"
+  },
+  {
+    "revision": "d130b408be093b2a227451b142c45162",
+    "url": "/ElearningReact/static/media/avatar_4.d130b408.jpg"
+  },
+  {
+    "revision": "f22d4b988775be9ba5e2f3ed0e0f973f",
+    "url": "/ElearningReact/static/media/avatar_5.f22d4b98.jpg"
+  },
+  {
+    "revision": "39860d1cd2e97c586b3e7a819b67ffb9",
+    "url": "/ElearningReact/static/media/avatar_6.39860d1c.jpg"
+  },
+  {
+    "revision": "b643b305d74e743854d0941ffc57cff3",
+    "url": "/ElearningReact/static/media/avatar_7.b643b305.jpg"
+  },
+  {
+    "revision": "384d6bce1039455a77b9a67b8a6198e1",
+    "url": "/ElearningReact/static/media/avatar_8.384d6bce.jpg"
+  },
+  {
+    "revision": "92645120e6839b60c1a14df08890ec90",
+    "url": "/ElearningReact/static/media/avatar_9.92645120.jpg"
+  },
+  {
+    "revision": "6245c7c2120010aa77c0218f6fc05d22",
+    "url": "/ElearningReact/static/media/cuuthanhvien.6245c7c2.png"
+  },
+  {
+    "revision": "9e647eb9d02c773e348bdc89fca6d495",
+    "url": "/ElearningReact/static/media/doat-giai-truong.9e647eb9.png"
+  },
+  {
+    "revision": "81e7be40617964e1c2c41d451d54c05e",
+    "url": "/ElearningReact/static/media/ki-niem-2-nam.81e7be40.png"
+  },
+  {
+    "revision": "7e98786fbe3fa25c2b4c4a0b2047d2ce",
+    "url": "/ElearningReact/static/media/loading-red-blue.7e98786f.svg"
+  },
+  {
+    "revision": "5315983b3b617e1a861b9740a43f41b2",
+    "url": "/ElearningReact/static/media/loading.5315983b.svg"
+  },
+  {
+    "revision": "440e399a2496b3c277929217d3e17ca2",
+    "url": "/ElearningReact/static/media/thanh-vien-dat-diem-cao.440e399a.png"
+  },
+  {
+    "revision": "b3bc495e37dac4605573e3412ef752f1",
+    "url": "/ElearningReact/static/media/thanh-vien-soi-noi.b3bc495e.png"
+  },
+  {
+    "revision": "c64d97d6febcd0b0c71ffe8b627371af",
+    "url": "/ElearningReact/static/media/thanh-vien-tich-cuc.c64d97d6.png"
+  },
+  {
+    "revision": "358f54cf07fb80158481a7dfd5a5171b",
+    "url": "/ElearningReact/static/media/user-vip.358f54cf.png"
+  }
+]);
